38d6566cd3188670d1553d1f545b6075c22a774e
Luke Hampton <lukehampton6@gmail.com>
2021-08-04 20:42:03 -0400

    login rendering

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const renderRoutes = require('./render-routes.js')
 
 router.use("/api", apiRoutes);
+router.use('/', renderRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
